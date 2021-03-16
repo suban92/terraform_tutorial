770e640412c626f856a6a89e0508db729eb6ec99
Suban <51520227+suban92@users.noreply.github.com>
2021-03-16 09:59:22 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+  provider "aws" {
+  region     = "eu-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_instance" "EC2" {
+  ami           = var.ami-id
+  instance_type = var.instance-type
+  key_name      = var.pem-key
+}
